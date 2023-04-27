9a9d244adcb8f0c56f10e1166d21e07d80f92104
timmoac <timmo.achsel@mambaby.com>
2023-04-27 14:39:47 +0200

    add folder strcuture, app route

--- sls-defect-detection-web.py
+++ sls-defect-detection-web.py
@@ -1,1 +1,18 @@
-# TODO add flask server
+from flask import Flask, render_template, request
+import tensorflow as tf
+import numpy as np
+import os
+
+# Flask Server
+app = Flask(__name__)
+
+@app.route('/')
+
+# TODO add prediction using tf
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
