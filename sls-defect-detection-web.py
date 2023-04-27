6195f7c952f56c1e5bf01595f8cf2d801209f607
timmoac <timmo.achsel@mambaby.com>
2023-04-27 15:39:50 +0200

    add model select

--- sls-defect-detection-web.py
+++ sls-defect-detection-web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import tensorflow as tf
 import numpy as np
 import os
@@ -6,13 +6,23 @@ import os
 # Flask Server
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+def model_select():
 
-# TODO add prediction using tf
-def home():
-    return render_template('index.html')
+    models = []
+    for filename in os.listdir('models'):
+        filepath = os.path.join('models', filename)
+        if os.path.isfile(filepath):
+            models.append(filepath)
 
+    return redirect(url_for('prediction'))
 
+@app.route('/predict', methods=['POST', 'GET'])
+def prediction():
+    if request.method == 'GET':
+        model = request.form['Model']
+
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, threaded=True)
