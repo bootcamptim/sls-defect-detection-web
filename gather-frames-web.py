080e2ca2399b5e8a89c8311336ac453b4b19047e
Timmo Achsel <timmo.ac@gmail.com>
2023-05-08 14:15:07 +0200

    add multithreading; minimum filesize

--- gather-frames-web.py
+++ gather-frames-web.py
@@ -1,51 +1,63 @@
 import os
 import shutil
-import filecmp
 import time
+import threading
+from queue import Queue
+import concurrent.futures
 
-# /Volumes/fhcwdev's home/sls-defect-detection-pi/output
-
-# intialize paths
 pi_dir = os.path.join('/Volumes', "fhcwdev's home", 'sls-defect-detection-pi', 'output')
 dest_dir = os.path.join('output')
 
 def get_files(directory):
     return set([f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))])
 
+def copy_file(file):
+    try:
+        src_file = os.path.join(pi_dir, file)
+        dst_file = os.path.join(dest_dir, file)
 
-def copy_files(network_files):
-    for file in network_files:
-        try:
-            src_file = os.path.join(pi_dir, file)
-            dst_file = os.path.join(dest_dir, file)
+        if os.path.exists(dst_file):
+            pass
+        else:
+            shutil.copy2(src_file, dst_file)
+            print(f'Copied {dst_file}')
 
-            # Check if file exists in local folder
-            if os.path.exists(dst_file):
-                pass
-                   
-            else:
-                # If file does not exist copy
-                shutil.copy2(src_file, dst_file)
-                print(f'Copied {dst_file}')
+    except Exception as e:
+        print(f"Error occurred while copying file: {e}")
 
-        except Exception as e:
-            print(f"Error occurred while copying file: {e}")
-
-last_seen_files = get_files(pi_dir)
-
-try:
-
-    print('Waiting for Files...')
+def poll_directory(queue, polling_interval=1):
+    last_seen_files = get_files(pi_dir)
 
     while True:
         current_files = get_files(pi_dir)
         new_files = current_files - last_seen_files
 
         if new_files:
-            copy_files(new_files)
+            # Sort files by creation time
+            sorted_files = sorted(new_files, key=lambda f: os.path.getctime(os.path.join(pi_dir, f)))
+            queue.put(sorted_files)
 
         last_seen_files = current_files
-        time.sleep(0.01)
+        time.sleep(polling_interval)
+
+polling_interval = 0.5  # Polling interval in seconds
+file_queue = Queue()
+
+# Start a separate thread to poll the network drive
+polling_thread = threading.Thread(target=poll_directory, args=(file_queue, polling_interval))
+polling_thread.daemon = True
+polling_thread.start()
+
+print('Waiting for Files...')
+
+try:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        while True:
+            # Process new files if available
+            if not file_queue.empty():
+                new_files = file_queue.get()
+                executor.map(copy_file, new_files)
+            time.sleep(0.01)
 
 except KeyboardInterrupt:
     print('\nAborted.')
--- sls-defect-detection-web.py
+++ sls-defect-detection-web.py
@@ -6,6 +6,9 @@ import numpy as np
 import os
 import cv2
 import glob
+import queue
+import threading
+import time
 
 # Flask Server
 app = Flask(__name__)
@@ -14,6 +17,24 @@ socketio = SocketIO(app)
 imdir = 'output'
 model_dir = 'models'
 loaded_models = {}
+img_queue = queue.Queue(maxsize=10)
+
+def image_queue_worker():
+    while True:
+        list_of_files = glob.glob(f'{imdir}/*')
+        if list_of_files:
+            latest_files = sorted(list_of_files, key=os.path.getctime, reverse=True)
+            for latest_file in latest_files:
+                file_size = os.path.getsize(latest_file)
+                if file_size > 50000:  # Adjust the threshold as needed
+                    img = cv2.imread(latest_file)
+                    if img is not None and np.any(img):
+                        try:
+                            img_queue.put(img, block=False)
+                        except queue.Full:
+                            pass
+                        break
+        # time.sleep(0.05)
 
 @app.route('/')
 def index():
@@ -21,12 +42,10 @@ def index():
     return render_template('index.html', models=models)
 
 @app.route('/video_feed')
-@app.route('/video_feed')
 def video_feed():
     model_name = request.args.get('model')
     print(model_name)
 
-    # Load the model if it has not been loaded yet
     if model_name not in loaded_models:
         model_path = os.path.join(model_dir, model_name)
         loaded_models[model_name] = tf.keras.models.load_model(model_path)
@@ -35,31 +54,26 @@ def video_feed():
 
     def generate():
         while True:
-            list_of_files = glob.glob(f'{imdir}/*')  
-            if list_of_files:  # if there are files
-                latest_files = sorted(list_of_files, key=os.path.getctime, reverse=True)  # get all files sorted by time
-                for latest_file in latest_files:  
-                    img = cv2.imread(latest_file)  
-                    if img is not None and np.any(img):  # if the image is readable and not empty
-                        # Preprocess the image for the model
-                        img_resized = cv2.resize(img, (640, 480))  
-                        img_expanded = np.expand_dims(img_resized, axis=0)
+            try:
+                img = img_queue.get(block=True, timeout=1)
+                img_resized = cv2.resize(img, (640, 480))
+                img_expanded = np.expand_dims(img_resized, axis=0)
+                prediction = model.predict(img_expanded)
+                result = "Defect" if prediction == 1 else "Ok"
+                socketio.emit('prediction', {'data': result})
 
-                        # Make a prediction
-                        prediction = model.predict(img_expanded)
-                        result = "Defect" if prediction == 1 else "Ok" 
-                        socketio.emit('prediction', {'data': result})  # send the prediction to the client                      
+                (flag, encodedImage) = cv2.imencode(".jpg", img)
+                if not flag:
+                    continue
+                yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                       bytearray(encodedImage) + b'\r\n')
+            except queue.Empty:
+                pass
 
-                        (flag, encodedImage) = cv2.imencode(".jpg", img)  # encode the image
-                        if not flag:
-                            continue
-                        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-                              bytearray(encodedImage) + b'\r\n')  # yield the image data
-                        break
-                
     return Response(generate(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 if __name__ == '__main__':
+    img_queue_worker_thread = threading.Thread(target=image_queue_worker)
+    img_queue_worker_thread.start()
     socketio.run(app, host='0.0.0.0', port=5500)
